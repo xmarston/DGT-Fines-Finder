d5b8e7cf8449be835b790c3f366d11443bf779ba
Ricardo Cámara <rcamara@netquest.com>
2019-08-30 10:43:32 +0200

    gemify v2

--- lib/finder.rb
+++ lib/finder.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module Finder
-  require 'finder/scrapper'
-  require 'finder/response_parser'
+  require_relative './finder/scrapper'
+  require_relative './finder/response_parser'
+
+  def self.fine?(text = '')
+    ResponseParser.new(Scrapper.new.scrap(text)).fine?
+  end
 end
